b937c440052bc34df29b65f03e5763cad4643de0
stefan-kanazir <kanazir.stefan@gmail.com>
2021-11-16 00:40:35 +0100

    React routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+// pages
+import AllPosts from "./pages/AllPosts"
+import Post from "./pages/Post";
+
 
 function App() {
-  return (
-    <h1>
-      Blog website
-    </h1>
-  );
+	return (
+		<BrowserRouter>
+			{/* Header Area */}
+			<div className="max-w-full">
+				{/* <Header /> */}
+			</div>
+
+			{/* Route Area */}
+			<Routes>
+        <Route element={<AllPosts />} path="/" exact />
+        <Route element={<Post />}  path="/:slug" />
+			</Routes>
+
+			{/* Footer Area */}
+			<div className="max-w-full">
+				{/* <Footer /> */}
+			</div>
+
+		</BrowserRouter>
+	);
 }
 
 export default App;
